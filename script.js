0e419d55f1ac0cc09246ad0cef75aae5b103c801
Scott Fennell <slfennell01@gmail.com>
2024-06-25 14:27:17 -0400

    Implement data parsing and filtering from CSV

---
+++ script.js
@@ -0,0 +1,15 @@
+// U86364709
+
+function main(){
+    // d3 code goes here
+
+    let elements,
+    stock,
+    startDate,
+    endDate,
+    array;
+
+    d3.dsv(",","data/mock_stock_data.csv").then(function(data){
+
+    })
+}
